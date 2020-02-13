d2a883a5a89ace701de1da216d4f854de8206c8e
pgambino-espear <pgambino@espear.com>
2020-02-13 13:30:48 -0500

    Set up server for deployment.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    const songs = [{id: 1, name:"Final Countdown"}];
+    
+    res.status(200).json(songs);
+})
+
+//make the port dyanmic
+const port = process.env.PORT || 5000;
+server.listen(port, () => 
+console.log(`\n** Running on port ${port} **n\``))
